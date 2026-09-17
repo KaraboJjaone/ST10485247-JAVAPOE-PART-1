cdabe85801c425d0a7fbae39d819cadb162c5b1c
Student <Student@LabVM2049939.hkknxq53lqsurix2r0qetcjyge.jnbx.internal.cloudapp.net>
2026-09-17 11:54:15 +0200

    Rename scanner to Myinput for better clarity

--- src/main/java/com/mycompany/jjaonepart1/JjaonePart1.java
+++ src/main/java/com/mycompany/jjaonepart1/JjaonePart1.java
@@ -12,52 +12,52 @@ import java.util.Scanner;
 public class JjaonePart1 {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner myInput = new Scanner(System.in);
         Login login = new Login();
 
         //Ask user to enter first Name
         System.out.print("Please enter your first name: ");
-        login.setFirstName(scanner.next());
+        login.setFirstName(myInput.next());
 
         //Ask user to enter last name
         System.out.print("Please enter your last name: ");
-        login.setLastName(scanner.next());
+        login.setLastName(myInput.next());
 
         //Ask the user to enter username and keep asking if it's wrongly formatted
         System.out.print("Please enter your username: ");
-        login.setUserName(scanner.next());
+        login.setUserName(myInput.next());
 
         while (!login.checkUserName()) {
             System.out.println("Username is not correctly formatted; please"
                     + " ensure thet your username contains an under"
                     + "underscore and is no more than five characters in length");
             System.out.print("Please enter your username: ");
-            login.setUserName(scanner.next());
+            login.setUserName(myInput.next());
         }
         System.out.println("Username successfully captured.");
 
         //Ask the user to enter password and keep asking if it's wrongly formatted
         System.out.print("Please enter your password: ");
-        login.setPassword(scanner.next());
+        login.setPassword(myInput.next());
 
         while (!login.checkPasswordComplexity()) {
             System.out.println("Password is not correctly formatted; please ensure that"
                     + "the password contains at least eight characters, a capital letter, "
                     + "a number, and a special character.");
             System.out.print("Please enter your password: ");
-            login.setPassword(scanner.next());
+            login.setPassword(myInput.next());
         }
         System.out.println("Password successfully captured.");
 
         //Ask the user to enter cell phone and keep asking if it's wrong
         System.out.print("Please enter your cell number: ");
-        login.setCellNumber(scanner.next());
+        login.setCellNumber(myInput.next());
 
         while (!login.checkCellPhoneNumber()) {
             System.out.println("Cell phone number is incorrectly formatted or does not contain"
                     + "international code.");
             System.out.print("Please enter your cell number: ");
-            login.setCellNumber(scanner.next());
+            login.setCellNumber(myInput.next());
         }
         System.out.println("Cell phone number successfully added.");
 
@@ -68,10 +68,10 @@ public class JjaonePart1 {
         System.out.println("\n--- Login ---");
 
         System.out.print("Enter your username: ");
-        String enteredUser = scanner.next();
+        String enteredUser = myInput.next();
 
         System.out.print("Enter your password: ");
-        String enteredPass = scanner.next();
+        String enteredPass = myInput.next();
 
         boolean loggedIn = login.loginUser(enteredUser, enteredPass);
         System.out.println(login.returnLoginStatus(loggedIn));
@@ -79,10 +79,10 @@ public class JjaonePart1 {
         //Keep asking for login details if they were incorrect
         while (!loggedIn) {
             System.out.print("Enter your username: ");
-            enteredUser = scanner.next();
+            enteredUser = myInput.next();
 
             System.out.print("Enter your password: ");
-            enteredPass = scanner.next();
+            enteredPass = myInput.next();
 
             loggedIn = login.loginUser(enteredUser, enteredPass);
             System.out.println(login.returnLoginStatus(loggedIn));
